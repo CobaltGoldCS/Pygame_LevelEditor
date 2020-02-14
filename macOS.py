d6a51c9a6db74882be2bddbe9676667844c0ce74
CobaltGoldCS <54545769+CobaltGoldCS@users.noreply.github.com>
2020-02-14 09:44:52 -0700

    Add files via upload

--- macOS.py
+++ macOS.py
@@ -1,58 +1,59 @@
 import pygame
 import os 
 from tkinter import filedialog, Tk, messagebox
-
-class levelEditor():
+# Need to find a way to fix needing the file to save
+class levelEditor:
     def __init__(self):
         self.first = True
         self.running = True
         self.loaded = False
-        self.win = pygame.display.set_mode((600, 800))
-        if os.path.exists(os.getcwd()+"\levels"):
+        WIDTH = int(input("What is the width: "))
+        HEIGHT = int(input("What is the height: "))
+        self.win = pygame.display.set_mode((WIDTH, HEIGHT))
+        if os.path.exists("levels"):
             pass
         else:
-            os.mkdir(os.getcwd()+"\levels")
-    def draw_Rect(self, pos1, pos2, color=(255, 255, 255)):
+            os.mkdir("levels")
+    def draw_Rect(self, pos1, pos2, color= (255,255,255)):
+        
         fX, fY, sX, sY = pos1[0], pos1[1], pos2[0], pos2[1]
         height = max(fY, sY) - min(fY, sY)
         width = max(fX, sX) - min(fX, sX)
         # Min is for always giving the corner
         pygame.draw.rect(self.win, color, (min(fX, sX), min(fY, sY), width, height), False)
-        return min(fX, sX), min(fY, sY), width, height
-    def loadFile(self, lfile):
-        """Takes an 'open' object and displays all of the rectangles"""
-        for line in lfile:
-            ituple = []
-            for item in line.split(" "):
-                num = int(item)
-                ituple.append(num)
-            pygame.draw.rect(self.win, (255,255,255), ituple, False)
-        pygame.display.update()
+        return min(fX, sX), min(fY, sY), width, height, color[0], color[1], color[2]
     def mainloop(self):
         self.win.fill((0, 0, 0))
         # Vars for while loop
-        rlist = []
         pygame.init()
+        Level = None
         while self.running:
+            pygame.display.update()
+
+            if Level != None:
+                for rect in Level.rlist:
+                    pygame.draw.rect(self.win,
+                                     (rect[4], rect[5], rect[6]),
+                                     (rect[0], rect[1], rect[2], rect[3]), False)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         sure = input("Do you wish to save?(y or n): ")
                         if sure == "y" and self.loaded == False:
-                            if len(rlist) != 0:
-                                numFiles = len(os.listdir("levels")
+                            if len(self.rlist) != 0:
+                                numFiles = len(os.listdir("levels"))
                                 os.chdir("levels")
-                                wFile = open("level%s"%str(numFiles+1), 'a')
-                                for line in rlist:
-                                    wFile.write(' '.join(str(value) for value in line) + '\n')
+                                wFile = open("level%s"%str(numFiles), 'a')
+                                for line in Level.rlist:
+                                    wFile.write('P '+' '.join(str(value) for value in line) + '\n')
                                 wFile.close()
                         pygame.quit()
-                        running = False
+                        self.running = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         for level in os.listdir("levels"):
                             print(level)
-                        file = open(os.getcwd()+"/levels/"+input("Filepath: "))
-                        self.loadFile(file)
+                        Level = decoder("levels/level%s"%input("Choose a level Number: "))
                         self.loaded = True
                 if event.type == pygame.MOUSEBUTTONUP:
                     if self.first:
@@ -61,10 +62,21 @@ class levelEditor():
                     else:
                         pos2 = pygame.mouse.get_pos()
                         self.first = True
-                        rlist.append(self.draw_Rect(pos1, pos2))
+                        Level.rlist.append(self.draw_Rect(pos1, pos2))
                         pygame.display.update()
                         self.loaded = False
-
+class decoder:
+    def __init__(self, lFile):
+        """Takes an 'open' object and displays all of the rectangles"""
+        self.rlist = []
+        lfile = open(lFile)
+        for line in lfile:
+            if line.startswith("P"):
+                self.rectangle(line)
+    def rectangle(self, line):
+        ituple = [int(item) for item in line.split()[1:]]
+        print(ituple)
+        self.rlist.append(ituple)
 if __name__ == "__main__":
     app = levelEditor()
     app.mainloop()
